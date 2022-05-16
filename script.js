e88dcb35efab6ee237d2f33b0bb02da940a7b214
emTr0 <em@emtr0.com>
2022-05-15 21:27:50 -0700

    Final changes made for completed project

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+// Computes interest based on user input, and displays results.
+
+function compute(){
+    var principal = document.getElementById("principal").value;
+
+    if(principal == "" || principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    } else {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"; 
+    } 
 }
-        
+
+// Updates displayed interest rate based on position of slider.
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
